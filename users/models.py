925e451b9c621e023e74db7f2b0fd3af283278ec
RomanZemlin <roman.zemlin@mail.ru>
2023-11-13 17:17:03 +0300

    h_w 26.1

--- users/models.py
+++ users/models.py
@@ -1,22 +1,8 @@
-from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class User(AbstractUser):
-    groups = models.ManyToManyField(
-        Group,
-        verbose_name='groups',
-        blank=True,
-        help_text='The groups this user belongs to.',
-        related_name='users_user_set'
-    )
-    user_permissions = models.ManyToManyField(
-        Permission,
-        verbose_name='user permissions',
-        blank=True,
-        help_text='Specific permissions for this user.',
-        related_name='users_user_set'
-    )
     username = None
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=25, blank=True, null=True)
